e19e697b830925008eadd1d3bbe9a7c8514d7853
Nathan louissaint <louissaintmark@gmail.com>
2023-05-25 10:23:16 -0400

    search

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,61 +1,31 @@
 import "./App.css"
-import Function from "./Function.jsx"
+import { Users } from "./users"
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-    
-<h1>
-  search
-</h1>
-<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for state.." title="Type in a state"/>
+function App(){
+  const[query, setQuery] = useState("")
+
+  // console.log(Users.filter(user => user.first_name.toLowerCase().includes("fe")))
+
+  return(
+    <div className="app">
+      <input type="text" placeholder="search" className="search"
+      onChange={(e)=> setQuery(e.target.value)}/> 
+      <ul className="list">
+        {Users.filter(user=>user.first_name.toLocaleLowerCase().includes(query)).map((user)=>(
+          <li key={user.id}className="listItem">{user.first_name}</li>
+        ))}
+        <li className="listItem">John</li>
+       
 
 
-<table id="myTable">
-  <tr class="header">
-    <th>State</th>
-    <th>State Epi</th>
-    <th>Title</th>
-  </tr>
-  <tr>
-    <td>Ak</td>
-    <td></td>
-  </tr>
-  <tr>
-    <td>AL</td>
-    <td>AL</td>
-  </tr>
-  <tr>
-    <td>AR</td>
-    <td>AR</td>
-  </tr>
-  <tr>
-    <td>AZ</td>
-    <td>AZ</td>
-  </tr>
-  <tr>
-    <td>CA</td>
-    <td>CA</td>
-  </tr>
-  <tr>
-    <td>CO</td>
-    <td>CO</td>
-  </tr>
-  <tr>
-    <td>CO</td>
-    <td>CO</td>
-  </tr>
-  <tr>
-    <td>DC</td>
-    <td>DC</td>
-  </tr>
-</table>
-      </header>
- 
- 
+       
+
+
+      </ul>
+   
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
